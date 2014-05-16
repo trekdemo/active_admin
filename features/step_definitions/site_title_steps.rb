308f51b5c1f48241419541b9d904421f0a73c534
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2014-05-16 11:05:22 +0200

    Fix site_title_steps.rb

--- features/step_definitions/site_title_steps.rb
+++ features/step_definitions/site_title_steps.rb
@@ -7,9 +7,9 @@ Then /^I should not see the site title "([^"]*)"$/ do |title|
 end
 
 Then /^I should see the site title image "([^"]*)"$/ do |image|
-  expect(page).to have_css 'h1#site_title img', src: image
+  expect(page).to have_css "h1#site_title img[src='#{image}']"
 end
 
 Then /^I should see the site title image linked to "([^"]*)"$/ do |url|
-  expect(page).to have_css 'h1#site_title a', href: url
+  expect(page).to have_css "h1#site_title a[href='#{url}']"
 end
